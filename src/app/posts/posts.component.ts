44b6e242b0386494edb0c119dc3dab16d1ee4ce3
Fabrice Berthet <berthet-fabrice@hotmail.fr>
2023-07-23 13:22:51 +0200

    dates

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Post } from '../models/post';
 import { PostService } from '../services/post.service';
 import { environment } from 'src/environments/environment.development';
+import { formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-posts',
